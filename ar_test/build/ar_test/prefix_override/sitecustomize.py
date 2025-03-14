3cc962d61490e5ebde2381fec363294a915c3b70
ex22544BlakeTan <163015523+ex22544BlakeTan@users.noreply.github.com>
2025-03-14 18:15:19 +0000

    Delete ar_test directory

--- ar_test/build/ar_test/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ex22544/ros2_ws/src/ar_test/install/ar_test'
--- ar_test/build/ar_test/share/ar_test/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/ex22544/ros2_ws/src/ar_test/build/ar_test"
